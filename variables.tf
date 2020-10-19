0bdcebaf5c369bd258cf2214f24c4d8ce14e0a5c
Marco Moioli <58217723+mmoioli@users.noreply.github.com>
2020-10-19 17:22:05 +0200

    First commit

---
+++ variables.tf
@@ -0,0 +1,34 @@
+# azure region
+variable "location" {
+  type = string
+  description = "Azure region where resources will be created"
+  default = "east us"
+}
+
+# resource group
+variable "resourcegroup" {
+  type = string
+  description = "The resource group for WVD Workspace/Host Pool/App Group deployment"
+  default = "TESTTerrformWVD"
+}
+
+# Workspace 
+variable "workspace" {
+  type = string
+  description = "The name of the workspace"
+  default = "TerraformWS"
+}
+
+# Host Pool 
+variable "hostpool" {
+  type = string
+  description = "The name of the Host Pool"
+  default = "TerraformHP"
+}
+
+# Host Pool 
+variable "appgroup" {
+  type = string
+  description = "The name of the App Group"
+  default = "TerraformAP"
+}
