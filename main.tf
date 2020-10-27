f23344674b24821f79a7f0b5fa7872293ef95cb9
Marco Moioli <58217723+mmoioli@users.noreply.github.com>
2020-10-27 11:43:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "MMoioli"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "MMoioliTerraform"
+         }
+       }
+     }
+
 provider "azurerm" {
   version = "~>2.0"
   features {}
